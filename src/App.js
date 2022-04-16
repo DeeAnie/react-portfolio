60680ded80b51c9a734d38a5b412d8c979eaa658
Diana <diana.zhusupbekova.kk@gmail.com>
2022-04-16 15:32:04 +0600

    Add setup files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./app.scss";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello React</h1>
+      <h2>Welcome to React</h2>
+    </div>
+  );
+}
+
+export default App;
